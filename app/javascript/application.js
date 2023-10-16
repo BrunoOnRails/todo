6fd84ed682d154a536be51c6351d059e867af8c9
Bruno <bruno@DESKTOP-R0GHACN>
2023-10-16 18:00:28 -0300

    bootstrap custom

---
+++ app/javascript/application.js
@@ -0,0 +1,15 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails";
+import "./controllers";
+import "@popperjs/core";
+import * as bootstrap from "bootstrap";
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
